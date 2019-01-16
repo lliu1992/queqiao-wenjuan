292e77dae2b6b58f496ea3450a62debc96d6b8a5
lzj <zj.liu@idiaoyan.com>
2019-01-16 23:22:24 +0800

    添加路由

--- urls.py
+++ urls.py
@@ -9,5 +9,8 @@ def make_app():
     return tornado.web.Application([
         (r'/test/?', view.TestHandler),
         (r'/?', view.IndexHandler),
+        (r'/upload_and_deploy/?', view.UpdateAndDeployHandler),
+        (r'/checkout_and_deploy/?', view.CheckoutBranchAndDeploy),
+        (r'/get_current_branch/?', view.GetCurrentBranchHandler),
 
     ])
